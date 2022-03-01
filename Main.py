d95a76f72a57f524ec2797120cacb2a687c1c54f
Vandewynckel Kristof <98815092+KristofVandewynckel@users.noreply.github.com>
2022-03-01 12:04:19 +0100

    Going through list of links

--- Main.py
+++ Main.py
@@ -22,20 +22,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Edge()
-
 link_list = []
 
-driver.get("https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=1&orderBy=relevance")
+for page in range(1,2):
+    url = 'https://www.immoweb.be/en/search/house/for-sale?countries=BE&page='+ str(page)+ '&orderBy=relevance'
 
-assert "Immoweb" in driver.title
-
-for listing in driver.find_elements_by_xpath("//article"):
-    print("ok")
+    driver.get(url)
     
-for elem in listing.find_elements_by_xpath("//a[@class='card__title-link']"):
-    link_list.append(elem.get_attribute("href"))
+    assert "Immoweb" in driver.title
 
-print(link_list)
+    for elem in driver.find_elements_by_xpath("//a[@class='card__title-link']"):
+        link_list.append(elem.get_attribute("href"))
 
 assert "No results found." not in driver.page_source
 driver.close()
+
+for each in link_list:
+    link = each
+    r = requests.get(link)
+    print(link, r.status_code)
+    soup = BeautifulSoup(r.content, "lxml")
+    soup.find_all("script")[1]
+    time.sleep(1)
+print(soup)
